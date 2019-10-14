7cbebe2114ef9b9d27cf5244375c3f530ad9bc0f
Benjamin Wolfe <bwolfe@esurance.com>
2019-10-14 15:54:40 -0500

    simplified colors and locations on timeline, trying out Calibri font, made a proper "images" directory, started tinkering with CSS

--- scripts/timeline.R
+++ scripts/timeline.R
@@ -8,12 +8,17 @@
 #' ---
 #' 
 #+ message=FALSE
+knitr::opts_chunk$set(
+  fig.path = "../images/"
+)
+
 library(tidyverse)
 library(lubridate)
 library(scales)
 library(here)
 library(dichromat)
 library(cowplot)
+library(extrafont)
 
 #' It's been helpful for me to write out my work history in code.
 #' For one thing, it's highly transparent, and I value that in my career search.
@@ -80,8 +85,8 @@ employers <- tibble(
     "Esurance"
   ),
   cities = c(
-    "Nashua",
-    "Pleasanton",
+    "Nashua, NH",
+    "Pleasanton, CA",
     "San Francisco; Austin"
   ),
   positions = c(
@@ -114,7 +119,7 @@ positions <-
   tribble(
     ~id, ~emp_id,  ~start_date,    ~end_date,  ~position,
       1,       1, "2004-08-01", "2006-06-01", "Spanish\nTeacher",
-      2,       2, "2006-10-01", "2011-10-14", "Mktg. Specialist\n/ Analyst",
+      2,       2, "2006-10-01", "2011-10-14", "Mktg.\nSpecialist\n/ Analyst",
       3,       3, "2011-10-17", "2012-11-07", "Marketing\nAnalyst",
       4,       3, "2012-11-07", "2014-04-01", "Sr. Marketing\nAnalyst",
       5,       3, "2014-04-01", "2015-09-23", "Manager,\nData Science",
@@ -159,8 +164,8 @@ teams_managed <-
     start_date = ymd(c("2013-03-11", "2015-09-23")),
     end_date   = ymd(c("2015-09-23", "2019-04-18")),
     type       = c("Data Science", "CX Analytics"),
-    label      = c("Led Marketing\nData Science Team",
-                   "Led CX\nAnalytics Team"),
+    label      = c("Led Marketing\nData Science Team\nSan Francisco, CA",
+                   "Led CX Analytics Team:\nAustin, TX (remote)"),
     details    = c("built, handed over", "promoted to, flattened")
   ) %>% 
   print()
@@ -235,7 +240,11 @@ people_managed <-
   ) %>% 
   print()
 
-#' Legend of locations (maybe tie colors to other timeline & education? maybe.):
+#' Legend of locations?
+#' (Maybe tie colors to other timeline & education? Maybe.)
+#' It is nice, and refreshing, to get a sense of _geography_ from a resume.
+#' However, is that something you really need to allocate page space to?
+#+ map
 map_data("state") %>% 
   mutate(
     region = region %>% 
@@ -258,10 +267,6 @@ map_data("state") %>%
 
 #' Notes on graphics...
 #' 
-#' Need to experiment with the right numbers for graphics device,
-#' so it doesn't show ugly lines (if I even use a gradient).
-#' These look very pretty with 324 x 799.
-#' 
 #' I tried for rounded rectangles:
 #' 
 #' * https://stackoverflow.com/a/52822082/573332
@@ -269,23 +274,15 @@ map_data("state") %>%
 #' 
 #' But they didn't work right with a reversed y-axis.
 #' 
-#' Also tried a fade on the right,
-#' but it just looked blurry, accidental, and dirty.
-#' 
-#' Finally, here's the StackOverflow link
+#' Here's the StackOverflow link
 #' to [flip the y-axis](https://stackoverflow.com/a/43626186/573332).
-#' 
-#' This is what I've got for now.
-#' Colors and proportions and such might change.
-#' Heck, I'm really open to changing any of it; this was a POC.
-#' But let's get everything else down, and then decide how to proceed.
 #' 
 #' Also under consideration...
 #' When I started working with what languages, and what projects I worked on?
 #' Awards, etc.?
 #' For the management section, include a concise summary, perhaps?
 #' 2 teams, 6 hires, 5 promoted?
-#+ fig.width = 472 / 96, fig.height = 799 / 96, dpi = 96
+#+ timeline, fig.width = 385 / 96, fig.height = 799 / 96, dpi = 96
 # reverse date transformation
 c_trans <- function(a, b, breaks = b$breaks, format = b$format) {
   
@@ -303,29 +300,28 @@ c_trans <- function(a, b, breaks = b$breaks, format = b$format) {
 rev_date <- c_trans("reverse", "date")
 
 # graphics parameters
-gradations <- 250
-decay      <- .25
-timeline_x         <- 0.23
+timeline_x         <- 0.20
 timeline_to_labels <- 0.03
-timeline_to_team   <- 0.08
+timeline_to_team   <- 0.06
 team_spacing       <- 0.05
-locations_x               <- .7
-locations_to_employer_box <- 0.05
-employer_box_to_team_box  <- 0.05
-box_to_name               <- 0.02
+right_edge         <- timeline_x + 0.4
+edge_to_team_box   <- 0.05
+box_to_name        <- 0.02
 
 # default text parameters on timeline itself
 update_geom_defaults("text", list(
-  size       = 3,
+  size       = 3.2,
   lineheight = 1,
   hjust      = 1,
-  vjust      = 0.75
+  vjust      = 0.8,
+  family     = "Calibri"
 ))
 
 ggplot() +
-  theme_nothing() +
+  theme_nothing() + # thematic elements
   theme(
     axis.text.y = element_text(
+      family        = "Calibri",
       face          = "italic",
       color         = "gray30",
       size          = 9,
@@ -337,7 +333,7 @@ ggplot() +
       inherit.blank = FALSE
     )
   ) +
-  scale_y_continuous(
+  scale_y_continuous( # scales: y, alpha, fill, shape
     trans    = rev_date,
     breaks   = ymd("2005-01-01") + years(0:14),
     labels   = function(x) format(x, "'%y"),
@@ -356,30 +352,20 @@ ggplot() +
       `transitioned in` = 16
     )
   ) +
-  geom_rect( # locations as colored bars
-    aes(
-      xmin  = locations_x - (alpha - 1) / gradations,
-      xmax  = locations_x -  alpha      / gradations,
-      ymin  = start_date,
-      ymax  = end_date,
-      fill  = region,
-      alpha = exp(-decay * alpha)
-    ),
-    data = crossing(work_locations, tibble(alpha = 1:gradations - 1))
-  ) +
   geom_rect( # employers as rectangles
     aes(
       xmin = 0,
-      xmax = locations_x - locations_to_employer_box,
+      xmax = right_edge,
       ymin = start_date,
       ymax = end_date
     ),
+    color = "white",
     alpha = 0.2,
     data  = employers
   ) +
   geom_text( # employer names
     aes(
-      x     = locations_x - locations_to_employer_box - box_to_name,
+      x     = right_edge - box_to_name,
       y     = start_date + weeks(11),
       label = paste(employer, cities, sep = "\n")
     ),
@@ -418,7 +404,12 @@ ggplot() +
   geom_text( # position names
     aes(
       x     = timeline_x - timeline_to_labels,
-      y     = padded_start,
+      y     = padded_start + 
+                case_when(
+                  id == 2  ~  weeks(3),
+                  id == 10 ~ -weeks(2),
+                  TRUE     ~  weeks(0)
+                ),
       label = position
     ),
     data = pad_ends(positions)
@@ -426,7 +417,7 @@ ggplot() +
   geom_rect( # teams as rectangles
     aes(
       xmin = timeline_x + timeline_to_team,
-      xmax = locations_x - locations_to_employer_box - employer_box_to_team_box,
+      xmax = right_edge - edge_to_team_box,
       ymin = start_date,
       ymax = end_date
     ),
@@ -435,10 +426,7 @@ ggplot() +
   ) +
   geom_text( # team labels
     aes(
-      x     = locations_x - 
-                locations_to_employer_box -
-                employer_box_to_team_box -
-                box_to_name,
+      x     = right_edge - edge_to_team_box - box_to_name,
       y     = start_date + weeks(12),
       label = label
     ),
@@ -475,7 +463,7 @@ ggplot() +
       y     = date,
       shape = event
     ),
-    color = "gray20",
+    color = "gray10",
     fill  = "gray",
     data  = people_managed %>% 
       filter(event %in% c("hired", "promoted", "transitioned in")) %>% 
